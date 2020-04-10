5a111c7b55a379eb3f0b963027bd9e9598e04517
rpdecks <rpdecks@gmail.com>
2020-04-09 22:27:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,10 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def initialize(id, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
 end
